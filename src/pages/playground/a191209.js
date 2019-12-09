19f2aae4316ce1aa600c75fefa1aeb2b5f91e9c4
caruisa <caruisa88@gmail.com>
2019-12-10 02:56:35 +0900

    no message

--- src/pages/playground/a191209.js
+++ src/pages/playground/a191209.js
@@ -4,6 +4,8 @@ import 'antd/dist/antd.css';
 import './playground.css';
 import { Comment, Avatar, Form, Button, List, Input } from 'antd';
 import moment from 'moment';
+import { Link } from "react-router-dom";
+import { PageHeader } from 'antd';
 
 const { TextArea } = Input;
 
@@ -73,6 +75,20 @@ class a191209 extends React.Component {
 
     return (
       <div>
+         <PageHeader
+        style={{
+          border: '1px solid rgb(235, 237, 240)',
+        }}
+        onBack={() => window.history.back()}
+        title="Timeline"
+        subTitle="(K . Y . W)"
+        extra={[
+          <div id="web_h_playground_btn_group">
+            <Link to="/mplayground"><button id="web_h_playground">뒤로가기 (React. Basic 놀이터)</button></Link>
+          </div>
+        ]}
+      />
+      <br />
         {comments.length > 0 && <CommentList comments={comments} />}
         <Comment
           avatar={
