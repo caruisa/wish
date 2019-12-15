fcf75622df31f93b41ba8d8dd5ee3699bf2815a4
caruisa <caruisa88@gmail.com>
2019-12-15 23:33:08 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wish/precache-manifest.11469af89f888e2a3d3107e96099b104.js"
+  "/wish/precache-manifest.2077c8dcbdfb3db865490debac535b7a.js"
 );
 
 self.addEventListener('message', (event) => {
